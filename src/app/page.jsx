f32310d489fe39a9ec1b31052f0f79335e921324
Alejandro Martir <hola@alemartir.com>
2024-10-23 23:35:57 -0600

    Update layout styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function HomePage() {
   return (
-    <div className="container mx-auto px-4 py-2 md:px-0">
+    <>
       <h1>Alejandro Mártir</h1>
-    </div>
+    </>
   );
 }
