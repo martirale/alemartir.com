8262e295b9046d0e7b3d69c1a320f52e753aff97
Alejandro Martir <hola@alemartir.com>
2024-10-23 22:28:36 -0600

    Add page layout styles

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -36,8 +36,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body>
-        <main>{children}</main>
+      <body className="bg-yellow text-black custom-vh">
+        <main className="w-full max-w-xl md:max-w-3xl lg:max-w-screen-2xl mx-auto pt-24 md:pt-28">
+          {children}
+        </main>
       </body>
     </html>
   );
