1e30a4bafbab38cb5a56cba122fbe31d683d0b48
Alejandro Martir <hola@alemartir.com>
2024-10-23 19:23:03 -0600

    Add image paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: process.env.LOCALHOST_HOSTNAME,
+        port: process.env.LOCALHOST_PORT,
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "strapi.alemartir.com",
+        port: "",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
