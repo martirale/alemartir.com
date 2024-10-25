947ed536598a41bcbe73487271dabec03ec146ef
Alejandro Martir <hola@alemartir.com>
2024-10-24 20:44:45 -0600

    Add new options

--- src/ui/menuOptions.js
+++ src/ui/menuOptions.js
@@ -3,7 +3,21 @@ export const menuOptions = [
     name: "Inicio",
     url: "/",
     showHeader: true,
-    showFooter: true,
+    showFooter: false,
+    showMisc: false,
+  },
+  {
+    name: "Privacidad",
+    url: "/privacidad",
+    showHeader: false,
+    showFooter: false,
+    showMisc: true,
+  },
+  {
+    name: "Términos de uso",
+    url: "/terminos",
+    showHeader: false,
+    showFooter: false,
     showMisc: true,
   },
 ];
