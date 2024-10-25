4efe3244872fd2d3c779827afe72ad17478e70ed
Alejandro Martir <hola@alemartir.com>
2024-10-24 21:33:32 -0600

    Add logo and description

--- src/ui/footer/FooterBrand.jsx
+++ src/ui/footer/FooterBrand.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Link from "next/link";
+import MonoAM from "@logos/MonoAM";
 
 export default function FooterBrand() {
   return (
     <div className="flex-1 border-b md:border-r md:border-b-0 px-4 pt-4 pb-24">
-      <>Columna Brand</>
+      <div className="flex flex-col">
+        <Link href="/" className="mb-8">
+          <MonoAM className="fill-black w-[149px] h-[72px]" />
+        </Link>
+
+        <p className="uppercase">
+          Diseñador gráfico publicitario y creador de contenido digital para
+          redes sociales
+        </p>
+      </div>
     </div>
   );
 }
