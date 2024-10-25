7103ca1eeeaf1c1fce081d66b45f18ca31927972
Alejandro Martir <hola@alemartir.com>
2024-10-25 14:55:58 -0600

    Update layout and message

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { getGlobal } from "@lib/api";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faSkull } from "@fortawesome/free-solid-svg-icons";
 
 export async function generateMetadata() {
   const globalData = await getGlobal();
@@ -18,6 +18,25 @@ export default function NotFound() {
   return (
     <>
       <h1>404 Not Found</h1>
+
+      <div className="flex flex-col md:flex-row w-full md:min-h-[calc(100vh-50%)]">
+        <div className="flex-1 md:flex-[50%] border-b md:border-r md:border-b-0">
+          <div className="flex items-center justify-center px-4">
+            <h2 className="text-7xl text-center py-32">
+              Ups, F... 404{" "}
+              <FontAwesomeIcon icon={faSkull} className="w-16 h-16" />
+            </h2>
+          </div>
+        </div>
+
+        <div className="flex-1 md:flex-[50%]">
+          <div className="flex items-center justify-center px-4">
+            <p className="text-2xl text-center py-16 md:py-32 md:mt-4">
+              Lo siento, pero parece que la página no existe...
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
