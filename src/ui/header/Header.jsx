525a1fbe03bf4e6b59d3e32460728cc9975456b6
Alejandro Martir <hola@alemartir.com>
2024-10-24 19:31:42 -0600

    Optimize border classes

--- src/ui/header/Header.jsx
+++ src/ui/header/Header.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 export default function Header() {
   return (
-    <header className="fixed w-full px-4 py-3 bg-yellow text-black border-b-black border-b-[1px] z-50">
+    <header className="fixed w-full px-4 py-3 bg-yellow text-black border-b z-50">
       <Navbar />
     </header>
   );
