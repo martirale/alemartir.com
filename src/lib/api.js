b676e43a3bfd8ef64b7fdfc82b305a270cce3c5e
Alejandro Martir <hola@alemartir.com>
2024-10-25 03:40:28 -0600

    Fix work by slug endpoint

--- src/lib/api.js
+++ src/lib/api.js
@@ -26,7 +26,7 @@ const fetchAPI = async (endpoint, options = {}) => {
 // METADATA GLOBAL
 export const getGlobal = async () => {
   const dataGlobal = await fetchAPI(
-    "/api/global?fields[0]=sitename&fields[1]=description",
+    `/api/global?fields[0]=sitename&fields[1]=description`,
     {
       cache: "no-store",
     }
@@ -37,7 +37,7 @@ export const getGlobal = async () => {
 // ABOUT PAGE
 export const getAbout = async () => {
   const dataAbout = await fetchAPI(
-    "/api/about?fields[0]=title&fields[1]=description&fields[2]=email&fields[3]=phone&populate[profile][fields][0]=url",
+    `/api/about?fields[0]=title&fields[1]=description&fields[2]=email&fields[3]=phone&populate[profile][fields][0]=url`,
     {
       cache: "no-store",
     }
@@ -48,7 +48,7 @@ export const getAbout = async () => {
 // PRIVACY PAGE
 export const getPrivacy = async () => {
   const dataPrivacy = await fetchAPI(
-    "/api/privacy?fields[0]=title&fields[1]=date&fields[2]=content",
+    `/api/privacy?fields[0]=title&fields[1]=date&fields[2]=content`,
     {
       cache: "no-store",
     }
@@ -59,7 +59,7 @@ export const getPrivacy = async () => {
 // TERMS PAGE
 export const getTerms = async () => {
   const dataTerms = await fetchAPI(
-    "/api/terms-of-use?fields[0]=title&fields[1]=date&fields[2]=content",
+    `/api/terms-of-use?fields[0]=title&fields[1]=date&fields[2]=content`,
     {
       cache: "no-store",
     }
@@ -69,21 +69,26 @@ export const getTerms = async () => {
 
 // WORKS INDEX
 export const getWorks = async () => {
-  const dataWorks = await fetchAPI(
-    "/api/works?fields[0]=title&fields[1]=slug&fields[2]=client&fields[3]=discipline&populate[cover][fields][0]=url&sort[0]=publishedAt:desc",
+  return fetchAPI(
+    `/api/works?fields[0]=title&fields[1]=slug&fields[2]=client&fields[3]=discipline&populate[cover][fields][0]=url&sort[0]=publishedAt:desc`,
     {
       cache: "no-store",
     }
   );
-  return dataWorks.data;
 };
 
-export const getWorkBySlug = async () => {
+export const getWorkBySlug = async (slug) => {
   const dataWorkBySlug = await fetchAPI(
-    "/api/works?filters[slug]=${slug}&fields[0]=title&fields[1]=slug&fields[2]=client&fields[3]=campaign&fields[4]=agency&fields[5]=country&fields[6]=discipline&fields[7]=creative&fields[8]=strategy&fields[9]=lead&fields[10]=design&fields[11]=copywriting&fields[12]=illustration&fields[13]=animation&fields[14]=photo&fields[15]=team&fields[16]=description",
+    `/api/works?filters[slug]=${slug}&fields[0]=title&fields[1]=slug&fields[2]=client&fields[3]=campaign&fields[4]=agency&fields[5]=country&fields[6]=discipline&fields[7]=creative&fields[8]=strategy&fields[9]=lead&fields[10]=design&fields[11]=copywriting&fields[12]=illustration&fields[13]=animation&fields[14]=photo&fields[15]=team&fields[16]=description`,
     {
       cache: "no-store",
     }
   );
-  return dataWorkBySlug.data;
+
+  if (dataWorkBySlug.data.length === 0) {
+    throw new Error("Work not found");
+  }
+
+  const dataWork = dataWorkBySlug.data[0];
+  return dataWork;
 };
