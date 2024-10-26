0148a73f9d896fd00183c8dedc8f450078563b1d
Alejandro Martir <hola@alemartir.com>
2024-10-25 23:44:57 -0600

    Create pagination component

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Pagination = ({ currentPage, totalPages }) => {
+  const pathname = usePathname();
+
+  return (
+    <div className="flex justify-center items-center">
+      <Link
+        href={currentPage > 1 ? `${pathname}?page=${currentPage - 1}` : "#"}
+        className={`flex-[35%] md:flex-1 p-4 ${currentPage <= 1 ? "opacity-50 cursor-not-allowed" : "hover:bg-yellow hover:text-black"}`}
+      >
+        Anterior
+      </Link>
+      {[...Array(totalPages)].map((_, i) => (
+        <Link
+          key={i}
+          href={`${pathname}?page=${i + 1}`}
+          className={`flex-[30%] md:flex-1 px-8 py-4 ${
+            currentPage === i + 1
+              ? "bg-yellow text-black"
+              : "hover:bg-yellow hover:text-black"
+          }`}
+        >
+          {i + 1}
+        </Link>
+      ))}
+      <Link
+        href={
+          currentPage < totalPages ? `${pathname}?page=${currentPage + 1}` : "#"
+        }
+        className={`flex-[35%] md:flex-1 p-4 ${currentPage >= totalPages ? "opacity-50 cursor-not-allowed" : "hover:bg-yellow hover:text-black"}`}
+      >
+        Siguiente
+      </Link>
+    </div>
+  );
+};
+
+export default Pagination;
