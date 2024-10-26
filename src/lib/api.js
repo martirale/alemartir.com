d5a879853b1352aad85f246e4aff5213b1cf2902
Alejandro Martir <hola@alemartir.com>
2024-10-25 23:44:42 -0600

    Add pagination in works index endpoint

--- src/lib/api.js
+++ src/lib/api.js
@@ -68,14 +68,17 @@ export const getTerms = async () => {
 };
 
 // WORKS INDEX
-export const getWorks = async () => {
+export const getWorks = async (page = 1, pageSize = 18) => {
   const dataWorks = await fetchAPI(
-    `/api/works?fields[0]=title&fields[1]=slug&fields[2]=client&fields[3]=discipline&populate[cover][fields][0]=url&sort[0]=publishedAt:desc`,
+    `/api/works?fields[0]=title&fields[1]=slug&fields[2]=client&fields[3]=discipline&populate[cover][fields][0]=url&sort[0]=publishedAt:desc&pagination[page]=${page}&pagination[pageSize]=${pageSize}`,
     {
       cache: "no-store",
     }
   );
-  return dataWorks.data;
+  return {
+    data: dataWorks.data,
+    meta: dataWorks.meta,
+  };
 };
 
 export const getWorkBySlug = async (slug) => {
