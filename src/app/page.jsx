4408b1671839be8514d45b624fa83ce945a70c05
Alejandro Martir <hola@alemartir.com>
2024-10-28 13:12:00 -0600

    Create hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,13 @@
-import WorksSection from "@components/sections/WorksSection";
 import React from "react";
+import HomeHero from "@components/hero/HomeHero";
+import WorksSection from "@components/sections/WorksSection";
 
 export default function HomePage() {
   return (
     <>
       <h1>Alejandro Mártir</h1>
 
-      {/* SECTIONS */}
+      <HomeHero />
       <WorksSection />
     </>
   );
