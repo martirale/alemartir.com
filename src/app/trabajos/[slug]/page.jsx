dfc778993800d7eeafdfa7c92036d3eeefa760ba
Alejandro Martir <hola@alemartir.com>
2024-10-28 14:30:11 -0600

    Fix container height in desktop

--- src/app/trabajos/[slug]/page.jsx
+++ src/app/trabajos/[slug]/page.jsx
@@ -76,113 +76,117 @@ export default async function WorkPage({ params }) {
       <>
         <h1>{title}</h1>
 
-        <div className="grid grid-cols-1 w-full p-4 border-b">
-          <div className="text-center md:mt-1">
-            <h2>{title}</h2>
+        <div className="md:-mt-[75px] md:pt-[75px] md:h-dvh">
+          <div className="grid grid-cols-1 w-full p-4 border-b">
+            <div className="text-center md:mt-1">
+              <h2>{title}</h2>
+            </div>
           </div>
-        </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-4 w-full md:min-h-[calc(100vh-50%)] border-b">
-          {/* MEDIA CONTENT */}
-          <section className="col-span-1 md:col-span-3 border-b md:border-r md:border-b-0">
-            {images.map((image) => (
-              <div key={image.id} className="w-full">
-                <Image
-                  src={`${process.env.STRAPI_API_URL}${image.url}`}
-                  alt={image.alternativeText}
-                  width={1920}
-                  height={1080}
-                  className="w-full h-auto"
-                />
+          <div className="grid grid-cols-1 md:grid-cols-4 w-full h-full border-b">
+            {/* MEDIA CONTENT */}
+            <section className="col-span-1 md:col-span-3 border-b md:border-r md:border-b-0">
+              {images.map((image) => (
+                <div key={image.id} className="w-full">
+                  <Image
+                    src={`${process.env.STRAPI_API_URL}${image.url}`}
+                    alt={image.alternativeText}
+                    width={1920}
+                    height={1080}
+                    className="w-full h-auto"
+                  />
+                </div>
+              ))}
+            </section>
+
+            {/* PROJECT INFO */}
+            <section className="col-span-1">
+              <div className="px-4 py-8 border-b">
+                <ContentRenderer blocks={description} />
               </div>
-            ))}
-          </section>
 
-          {/* PROJECT INFO */}
-          <section className="col-span-1">
-            <div className="px-4 py-8 border-b">
-              <ContentRenderer blocks={description} />
-            </div>
-
-            <div className="px-4 py-8">
-              <ul>
-                {client && (
-                  <li>
-                    <span className="font-bold">Cliente:</span> {client}
-                  </li>
-                )}
-                {campaign && (
-                  <li>
-                    <span className="font-bold">Campaña:</span> {campaign}
-                  </li>
-                )}
-                {agency && (
-                  <li>
-                    <span className="font-bold">Agencia:</span> {agency}
-                  </li>
-                )}
-                {country && (
-                  <li>
-                    <span className="font-bold">País:</span> {country}
-                  </li>
-                )}
-                {discipline && (
-                  <li>
-                    <span className="font-bold">Disciplina:</span> {discipline}
-                  </li>
-                )}
-                {creative && (
-                  <li>
-                    <span className="font-bold">Dirección creativa:</span>{" "}
-                    {creative}
-                  </li>
-                )}
-                {strategy && (
-                  <li>
-                    <span className="font-bold">Estrategia:</span> {strategy}
-                  </li>
-                )}
-                {lead && (
-                  <li>
-                    <span className="font-bold">Diseñador principal:</span>{" "}
-                    {lead}
-                  </li>
-                )}
-                {design && (
-                  <li>
-                    <span className="font-bold">Diseño:</span> {design}
-                  </li>
-                )}
-                {copywriting && (
-                  <li>
-                    <span className="font-bold">Copywriting:</span>{" "}
-                    {copywriting}
-                  </li>
-                )}
-                {illustration && (
-                  <li>
-                    <span className="font-bold">Ilustración:</span>{" "}
-                    {illustration}
-                  </li>
-                )}
-                {animation && (
-                  <li>
-                    <span className="font-bold">Animación:</span> {animation}
-                  </li>
-                )}
-                {photo && (
-                  <li>
-                    <span className="font-bold">Fotografía:</span> {photo}
-                  </li>
-                )}
-                {team && (
-                  <li>
-                    <span className="font-bold">Equipo de trabajo:</span> {team}
-                  </li>
-                )}
-              </ul>
-            </div>
-          </section>
+              <div className="px-4 py-8">
+                <ul>
+                  {client && (
+                    <li>
+                      <span className="font-bold">Cliente:</span> {client}
+                    </li>
+                  )}
+                  {campaign && (
+                    <li>
+                      <span className="font-bold">Campaña:</span> {campaign}
+                    </li>
+                  )}
+                  {agency && (
+                    <li>
+                      <span className="font-bold">Agencia:</span> {agency}
+                    </li>
+                  )}
+                  {country && (
+                    <li>
+                      <span className="font-bold">País:</span> {country}
+                    </li>
+                  )}
+                  {discipline && (
+                    <li>
+                      <span className="font-bold">Disciplina:</span>{" "}
+                      {discipline}
+                    </li>
+                  )}
+                  {creative && (
+                    <li>
+                      <span className="font-bold">Dirección creativa:</span>{" "}
+                      {creative}
+                    </li>
+                  )}
+                  {strategy && (
+                    <li>
+                      <span className="font-bold">Estrategia:</span> {strategy}
+                    </li>
+                  )}
+                  {lead && (
+                    <li>
+                      <span className="font-bold">Diseñador principal:</span>{" "}
+                      {lead}
+                    </li>
+                  )}
+                  {design && (
+                    <li>
+                      <span className="font-bold">Diseño:</span> {design}
+                    </li>
+                  )}
+                  {copywriting && (
+                    <li>
+                      <span className="font-bold">Copywriting:</span>{" "}
+                      {copywriting}
+                    </li>
+                  )}
+                  {illustration && (
+                    <li>
+                      <span className="font-bold">Ilustración:</span>{" "}
+                      {illustration}
+                    </li>
+                  )}
+                  {animation && (
+                    <li>
+                      <span className="font-bold">Animación:</span> {animation}
+                    </li>
+                  )}
+                  {photo && (
+                    <li>
+                      <span className="font-bold">Fotografía:</span> {photo}
+                    </li>
+                  )}
+                  {team && (
+                    <li>
+                      <span className="font-bold">Equipo de trabajo:</span>{" "}
+                      {team}
+                    </li>
+                  )}
+                </ul>
+              </div>
+            </section>
+          </div>
         </div>
 
         {/* BACK WORKS */}
