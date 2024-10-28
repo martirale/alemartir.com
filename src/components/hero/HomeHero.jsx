7849f3f5f36994e4ea9f268023b1892d2a0303d3
Alejandro Martir <hola@alemartir.com>
2024-10-28 15:05:02 -0600

    Update layout order

--- src/components/hero/HomeHero.jsx
+++ src/components/hero/HomeHero.jsx
@@ -4,8 +4,12 @@ export default async function HomeHero() {
   return (
     <>
       <section className="grid grid-cols-1 md:grid-cols-4 w-full border-b">
-        <div className="md:border-r md:-mt-[75px] md:pt-[75px] md:h-dvh">
-          <p>Lorem ipsum.</p>
+        <div className="col-span-1 md:border-r md:-mt-[75px] md:pt-[75px] md:h-dvh order-2 md:order-1">
+          <p>Lorem ipsum 1.</p>
+        </div>
+
+        <div className="col-span-1 md:col-span-3 order-1 md:order-2">
+          <p>Lorem ipsum 2.</p>
         </div>
       </section>
     </>
