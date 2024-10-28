84d85cb2200023cd0d4ebb23032616a16810fbec
Alejandro Martir <hola@alemartir.com>
2024-10-28 01:29:56 -0600

    Fix heading marging top

--- src/components/sections/Works.jsx
+++ src/components/sections/Works.jsx
@@ -14,7 +14,7 @@ export default async function WorksSection() {
       <>
         <section>
           <div className="flex flex-col md:flex-row w-full items-center p-4 border-b">
-            <div className="flex-1 text-center mt-2">
+            <div className="flex-1 text-center md:mt-1">
               <h2>Trabajos</h2>
             </div>
           </div>
