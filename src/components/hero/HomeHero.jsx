0909a498cfd77a0776b35944253960660424e887
Alejandro Martir <hola@alemartir.com>
2024-10-28 14:31:55 -0600

    Fix container height in desktop

--- src/components/hero/HomeHero.jsx
+++ src/components/hero/HomeHero.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default async function HomeHero() {
-  return <></>;
+  return (
+    <>
+      <section className="grid grid-cols-1 md:grid-cols-4 w-full border-b">
+        <div className="md:border-r md:-mt-[75px] md:pt-[75px] md:h-dvh">
+          <p>Lorem ipsum.</p>
+        </div>
+      </section>
+    </>
+  );
 }
