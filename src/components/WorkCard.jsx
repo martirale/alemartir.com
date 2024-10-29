6c8f7c33360d0479b2b4010c47a131429086eb93
Alejandro Martir <hola@alemartir.com>
2024-10-29 00:48:52 -0600

    Fix last border rigth

--- src/components/WorkCard.jsx
+++ src/components/WorkCard.jsx
@@ -13,10 +13,10 @@ export default function WorkCard({
   return (
     <div className={`relative border-b bg-yellow text-black group`}>
       {!isLastInRow && (
-        <div className="absolute top-0 right-0 h-full border-r border-black" />
+        <div className="absolute top-0 right-0 h-full border-r z-10" />
       )}
       <Link href={`/trabajos/${slug}`}>
-        <div className="w-full aspect-w-1 aspect-h-1 border-b">
+        <div className="w-full aspect-w-1 aspect-h-1 border-b relative z-0">
           <Image
             src={`${process.env.STRAPI_API_URL}${cover.url}`}
             alt={cover.url}
